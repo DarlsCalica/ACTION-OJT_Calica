472bd8677a80f4c337d21fba2076d1a2196eb789
Darlene Calica <darlene.calica@awsys-i.com>
2024-01-29 16:23:53 +0800

    lol

--- OJT_1-Calica/src/main.tsx
+++ OJT_1-Calica/src/main.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "./index.css"
 import { BrowserRouter, Route, Routes} from "react-router-dom"
-import Home from './pages/Home'
 import Calculator from './pages/Calculator'
-import JSON from './pages/Json'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
+import Home from './pages/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-// Routes for different Pages
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Routes>
-      <Route path="/home" element={<Home />}></Route>
-      <Route path="/JSON" element={<JSON />}></Route>
-      <Route path="/calculator" element={<Calculator />}></Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/calculator" element={<Calculator />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- OJT_1-Calica/src/pages/Home.tsx
+++ OJT_1-Calica/src/pages/Home.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <Container fluid>
+        <Row className="justify-content-md-center" >
+            <Col md="auto">
+              <div style={{backgroundPosition: 'center center'}}>
+                <h1>DIGITAL TRANSFORMATION</h1>
+              </div>
+            </Col>
+        </Row>
+    </Container>
   )
 }
