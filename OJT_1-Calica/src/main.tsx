81cca530700f9fdb30c7760c708c487667b3e72b
Darlene Calica <darlene.calica@awsys-i.com>
2024-01-29 15:37:57 +0800

    Added Route and import Bootstrap and router

--- OJT_1-Calica/src/main.tsx
+++ OJT_1-Calica/src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from './pages/Home'
+import Calculator from './pages/Calculator'
+import JSON from './pages/Json'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
+
+// Routes for different Pages
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Routes>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/JSON" element={<JSON />}></Route>
+      <Route path="/calculator" element={<Calculator />}></Route>
+    </Routes>
   </React.StrictMode>,
 )
