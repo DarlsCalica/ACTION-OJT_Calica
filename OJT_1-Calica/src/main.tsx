5e0917f3468d2b30c3b0ae86c2f73e14f43d760d
Darlene Calica <darlene.calica@awsys-i.com>
2024-01-30 16:20:23 +0800

    Added path for JSON and Hobbies

--- OJT_1-Calica/src/main.tsx
+++ OJT_1-Calica/src/main.tsx
@@ -5,13 +5,17 @@ import { BrowserRouter, Route, Routes} from "react-router-dom"
 import Calculator from './pages/Calculator'
 import Home from './pages/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Json from './pages/Json'
+import Hobbies from './pages/Hobbies'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/calculator" element={<Calculator />}></Route>
+        <Route path="/json_sample" element={<Json />}></Route>
+        <Route path="/hobbies" element={<Hobbies />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
