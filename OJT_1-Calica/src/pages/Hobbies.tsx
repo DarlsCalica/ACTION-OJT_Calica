175e0287c033a31d4de0434e29beaf1f1703e248
Darlene Calica <darlene.calica@awsys-i.com>
2024-01-30 18:05:25 +0800

    Created Initial Hobbies

--- OJT_1-Calica/src/pages/Hobbies.tsx
+++ OJT_1-Calica/src/pages/Hobbies.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Collapse, Container, Row } from 'react-bootstrap'
+
+
 
 export default function Hobbies() {
+    const [open, setOpen] = useState(false);
+
   return (
-    <div>Hobbies</div>
+   <Container fluid>
+        <Row>
+            <div className="aboutTitle">
+            ABOUT ME
+            </div>
+        </Row>
+        <Row style={{width: '90vh', margin: 'auto'}}>
+            <div className="intro">
+           <p> はじめまして!!!</p>  
+            わたし  の  なまえ  は  カリカ  ダルリン です。<br />
+            Baguio City から きました。<br />
+            わたし は 22歳 です。<br />
+            <Button className='trans'
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+      >
+        Translate
+      </Button>
+      <Collapse in={open}>
+        <div id="translation">
+         <p>Nice to meet you! </p>
+          My name is Darlene Calica <br />
+          I am from Baguio City <br />
+          I am 22 years old. <br />
+          </div>
+      </Collapse>
+            </div>
+        </Row>
+   </Container>
   )
 }
