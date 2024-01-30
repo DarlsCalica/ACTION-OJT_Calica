d72394e820465d8590e3dab3f1ad393dd8b2475a
Darlene Calica <darlene.calica@awsys-i.com>
2024-01-30 16:22:10 +0800

    Used components and changed style

--- OJT_1-Calica/src/pages/Home.tsx
+++ OJT_1-Calica/src/pages/Home.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Button, Card, Col, Container, Row } from 'react-bootstrap'
-import'./Home.css'
+import'./style/home.css'
+import { useNavigate } from 'react-router-dom';
+import NaviButton from './components/button';
+
 
 export default function Home() {
+
+  const navigate = useNavigate();
+
   return (
     <Container fluid='md'>
       {/* Title and Author */}
@@ -11,7 +17,7 @@ export default function Home() {
           DIGITAL TRANSFORMATION
         </div>
        </Row>
-       <Row >
+       <Row style={{width: '90vh', margin: 'auto'}}>
        <div className="author">
             Created By: Darlene Louise M. Calica
         </div>
@@ -20,34 +26,28 @@ export default function Home() {
       {/* Buttons */}
       <Row className='Buttons' style={{fontFamily: 'Pixelify Sans'}}>
       <Col xs={6} md={4}>
-           <Button variant="success" size="lg">
-            Calculator
-          </Button>{' '}
+           <NaviButton label="Calculator" onClick={() => navigate("/calculator")}/>
         </Col>
         <Col xs={6} md={4}>
-      
         </Col>
         <Col xs={6} md={4}>
-          <Button variant="success" size="lg">
-            JSON Sample
-          </Button>
+        <NaviButton label="JSON Sample" onClick={() => navigate("/json_sample")}/>
         </Col>
       </Row>
 
       {/* Hobbies */}
       <Row>
       <Card>
-      <Card.Header as="h5" style={{fontFamily: 'Workbench', fontSize: '30px'}}>ABOUT ME</Card.Header>
-      <Card.Body>
-        <Card.Title style={{fontFamily: 'Sixtyfour', fontSize: '20px'}}>My Hobbies</Card.Title>
-        <Card.Text style={{fontFamily: 'Barlow', fontSize: '18px', fontWeight: '400'}}>
-          What's up! My Name is Darlene Louise Calica, bit long? Aight, call me Darls <br></br>
-          Wanna know what my Hobbies are?
-        </Card.Text>
-        <Button variant="dark"  style={{fontFamily: 'Barlow', fontSize: '18px'}}>Yes, I wanna know! </Button>
-      </Card.Body>
-    </Card>
-    
+          <Card.Header as="h5" style={{fontFamily: 'Workbench', fontSize: '30px'}}>ABOUT ME</Card.Header>
+          <Card.Body>
+            <Card.Title style={{fontFamily: 'Sixtyfour', fontSize: '20px'}}>My Hobbies</Card.Title>
+            <Card.Text style={{fontFamily: 'Pixelify Sans', fontSize: '18px', fontWeight: '400'}}>
+              What's up! My Name is Darlene Louise Calica, bit long? Aight, call me Darls <br></br>
+              Wanna know what my Hobbies are?
+            </Card.Text>
+            <Button onClick={() =>navigate("/hobbies")} variant="dark"  style={{fontFamily: 'Pixelify Sans', fontSize: '18px'}}>Yes, I wanna know! </Button>
+          </Card.Body>
+        </Card>
       </Row>
     </Container>
   )
