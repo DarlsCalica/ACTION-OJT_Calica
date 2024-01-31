e9f7b0d845677345f0f960f13827a887dbd65a98
Darlene Calica <darlene.calica@awsys-i.com>
2024-01-31 18:51:05 +0800

    Polish CSS

--- OJT_1-Calica/src/pages/Home.tsx
+++ OJT_1-Calica/src/pages/Home.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { Button, Card, Col, Container, Row } from 'react-bootstrap'
+import { useState } from 'react'
+import {  Button, Col, Container, Row } from 'react-bootstrap'
 import'./style/style.css'
 import { useNavigate } from 'react-router-dom';
 import NaviButton from './components/button';
 
 
 export default function Home() {
-
   const navigate = useNavigate();
 
   return (
-    <Container fluid='md'>
-      {/* Title and Author */}
-       <Row>
+    <Container fluid>
+     {/* Title and Author */}
+     <Row style={{margin: 'auto'}}>
         <div className='title'>
           DIGITAL TRANSFORMATION
         </div>
@@ -22,33 +21,33 @@ export default function Home() {
             Created By: Darlene Louise M. Calica
         </div>
        </Row>
-
       {/* Buttons */}
-      <Row className='Buttons' style={{fontFamily: 'Pixelify Sans'}}>
-      <Col xs={6} md={4}>
-           <NaviButton label="Calculator" onClick={() => navigate("/calculator")}/>
-        </Col>
-        <Col xs={6} md={4}>
-        </Col>
-        <Col xs={6} md={4}>
-        <NaviButton label="JSON Sample" onClick={() => navigate("/json_sample")}/>
-        </Col>
+      <Row style={{width: '60vh' ,margin:'auto'}}>
+        <div className="Buttons">
+          <Col style={{}}>
+          <NaviButton label='Calculator' onClick={() => navigate("/calculator")}/>
+          </Col>
+          <Col>
+          <NaviButton label='JSON Sample' onClick={() => navigate("/json_sample")}/>
+          </Col>
+         </div>
       </Row>
 
-      {/* Hobbies */}
-      <Row>
-      <Card>
-          <Card.Header as="h5" style={{fontFamily: 'Workbench', fontSize: '30px'}}>ABOUT ME</Card.Header>
-          <Card.Body>
-            <Card.Title style={{fontFamily: 'Sixtyfour', fontSize: '20px'}}>My Hobbies</Card.Title>
-            <Card.Text style={{fontFamily: 'Pixelify Sans', fontSize: '18px', fontWeight: '400'}}>
-              What's up! My Name is Darlene Louise Calica, bit long? Aight, call me Darls <br></br>
+        {/* About me */}
+        <Row style={{width: '90vh', margin: 'auto'}}>
+     <div className="about">
+              <div className="abTitle" style={{fontFamily:'Workbench', fontSize:'30px', fontWeight:'400'}}>
+                 ABOUT ME
+                 <hr />
+              </div> 
+               <div className="desc" style={{fontFamily: 'Pixelify Sans', fontSize: '18px', fontWeight: '400'}}>
+                 What's up! <br /> My Name is Darlene Louise Calica, bit long? Aight, call me Darls <br />
               Wanna know what my Hobbies are?
-            </Card.Text>
-            <Button onClick={() =>navigate("/hobbies")} variant="dark"  style={{fontFamily: 'Pixelify Sans', fontSize: '18px'}}>Yes, I wanna know! </Button>
-          </Card.Body>
-        </Card>
-      </Row>
+               </div>
+               <Button onClick={() =>navigate("/hobbies")} variant="dark"  style={{fontFamily: 'Pixelify Sans', fontSize: '18px', marginTop:'18px'}}>Yes, I wanna know! </Button>
+        </div>
+       </Row>
+
     </Container>
   )
 }
