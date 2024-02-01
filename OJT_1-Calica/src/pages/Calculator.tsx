6776d489f16e63fb99bdb83de8379489a2b81dfc
Darlene Calica <darlene.calica@awsys-i.com>
2024-02-01 16:38:54 +0800

    For calculator page

--- OJT_1-Calica/src/pages/Calculator.tsx
+++ OJT_1-Calica/src/pages/Calculator.tsx
@@ -1,6 +1,24 @@
+import { Container, Row } from "react-bootstrap";
+import HomeButton from "./components/homebutton";
+import { useNavigate } from "react-router-dom";
 
 export default function Calculator() {
+
+  const navigate = useNavigate();
+
   return (
-    <div>Calculator</div>
+      <Container fluid>
+    {/* Home Button */}
+       <Row style={{width: '140px'}}>
+          <div style={{marginTop: '20px'}}>
+            <HomeButton label='Home' onClick={() => navigate("/")} />
+          </div>
+        </Row>
+
+    {/* Title */}
+        <Row>
+          
+        </Row>
+      </Container>
   )
 }
---
+++ OJT_1-Calica/src/pages/components/Form.tsx
@@ -0,0 +1,29 @@
+import Buttons from "./Buttons";
+
+interface Props {
+  output: string;
+  handleButtonClick: (value: string) => void;
+  reset?: () => void;
+  del?: () => void;
+  equalfn?: () => void;
+}
+
+const Form = ({ output, handleButtonClick, reset, del, equalfn }: Props) => {
+  return (
+    <form className="calculator" aria-label="Calculator">
+      <div className="screen" tabIndex={0}>
+        <output id="display" aria-live="polite">
+          {output}
+        </output>
+      </div>
+      <Buttons
+        handleButtonClick={handleButtonClick}
+        reset={reset}
+        del={del}
+        equalfn={equalfn}
+      />
+    </form>
+  );
+};
+
+export default Form;
